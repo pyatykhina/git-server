a3d920f017f2e8988deb752f49d56377f6fe32db
Татьяна Пятыхина <tpyatyhina@mail.ru>
2021-06-20 00:49:48 +0300

    footer component

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,16 +4,22 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Details from "./pages/Details";
 import History from "./pages/History";
 import Settings from "./pages/Settings";
+import Footer from "./components/Footer";
 
 function Router(props) {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={History} />
-        <Route path="/build/:number" component={Details} />
-        <Route path="/settings" component={Settings} />
-        <Redirect from='*' to='/' />
-      </Switch>
+      <div className="wrapper">
+        <main>
+            <Switch>
+              <Route path="/" exact component={History} />
+              <Route path="/build/:number" component={Details} />
+              <Route path="/settings" component={Settings} />
+              <Redirect from='*' to='/' />
+            </Switch>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+
+const Footer = ({ e }) => {
+    return (
+        <footer>
+            <div className="footer">
+                <nav className="footer__nav">
+                    <Link to="#" className="footer__nav-item">Support</Link>
+                    <Link to="#" className="footer__nav-item">Learning</Link>
+                    <Link to="#" className="footer__nav-item">Русская версия</Link>
+                </nav>
+                <div className="footer__copyright">
+                    &copy; 2021 Tatiana Pyatykhina
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
